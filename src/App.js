4b1833c5e45b3d3a406ccf98eb36a788c86f4cb6
sam0sva1 <konstantus@live.ru>
2017-02-05 23:20:18 +0300

    CNT add the reset button

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function updateState(state, action) {
   switch (action.type) {
     case 'INCREMENT': return { count: state.count + action.amount };
     case 'DECREMENT': return { count: state.count - action.amount };
+    case 'RESET': return { count: 0 };
     default: return state;
   }
 }
 
 const incrementAction = { type: 'INCREMENT', amount: 1 };
 const decrementAction = { type: 'DECREMENT', amount: 1 };
+const resetAction = { type: 'RESET' };
 
 const store = new Store(updateState, initialState);
 
@@ -23,6 +25,7 @@ export default class Counter extends Component {
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   componentDidMount() {
@@ -37,6 +40,10 @@ export default class Counter extends Component {
     store.update(decrementAction);
   }
 
+  reset() {
+    store.update(resetAction);
+  }
+
   render() {
     return (
       <div className="counter">
@@ -44,6 +51,7 @@ export default class Counter extends Component {
 
         <div className="buttons">
           <button className="buttons__decrement" onClick={this.decrement}>-</button>
+          <button className="buttons__reset" onClick={this.reset}>R</button>
           <button className="buttons__increment" onClick={this.increment}>+</button>
         </div>
       </div>
